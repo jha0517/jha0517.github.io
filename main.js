1688b50865f474f9e0d272958ff4f3b341293fac
Hyun Ah JUNG <jha0517@hotmail.com>
2026-03-30 00:49:26 +0100

    backup

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
-const THREE = window.MINDAR.IMAGE;
+// const THREE = window.MINDAR.IMAGE;
+
+// console.log("Three", THREE);
+// document.addEventListener("DOMContentLoaded", () => {
+//     const start = async() => {
+//         const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+//             container: document.body,
+//             imageTargetSrc: "./assets/mind/targets.mind",
+//         });
+
+//         const { renderer, scene, camera } = mindarThree;
+
+//         const geometry = new THREE.PlaneGeometry(1, 1);
+//         const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, transparent: true, opacity: 0.5 });
+//         const plane = new THREE.Mesh(geometry, material);
+
+//         const ancher = mindarThree.addAnchor(0);
+//         ancher.group.add(plane);
+        
+//         await mindarThree.start();
+
+//         renderer.setAnimationLoop(() => {
+//             renderer.render(scene, camera);
+//         });
+//     }
+//     start();
+// });
+const THREE = window.THREE;
+const { MindARThree } = window.MINDAR.IMAGE;
+
+
 
 console.log("Three", THREE);
-document.addEventListener("DOMContentLoaded", () => {
-    const start = async() => {
-        const mindarThree = new window.MINDAR.IMAGE.MindARThree({
-            container: document.body,
-            imageTargetSrc: "./assets/mind/targets.mind",
-        });
-
-        const { renderer, scene, camera } = mindarThree;
-
-        const geometry = new THREE.PlaneGeometry(1, 1);
-        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, transparent: true, opacity: 0.5 });
-        const plane = new THREE.Mesh(geometry, material);
-
-        const ancher = mindarThree.addAnchor(0);
-        ancher.group.add(plane);
-        
-        await mindarThree.start();
-
-        renderer.setAnimationLoop(() => {
-            renderer.render(scene, camera);
-        });
-    }
-    start();
-});
+console.log("MindARThree", MindARThree);
